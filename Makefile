211efef4258c3f0c997655ad336f3e4f52c0a8cf
Squirre1MAN <344900432@qq.com>
2015-10-14 23:46:42 +0800

    To Separate the site with react components.

--- Makefile
+++ Makefile
@@ -11,3 +11,26 @@ server:
 .PHONY: clean
 clean:
 	rm -r bundle
+
+.PHONY:babel
+babel:
+
+	#--------------------------------------------
+	# Compiles js/app.js
+	babel js/app.js --out-file js/app.jsx
+
+	#--------------------------------------------
+	#Make sure that the `build` directory exists.
+	mkdir -p build
+
+	#--------------------------------------------
+	# Compiles js/app.jsx
+	babel js/app.jsx --out-file build/app.js
+
+.PHONY:js
+js:
+	babel --watch js/app.js --out-file build/app.js
+
+.PHONY:all
+all:
+	(make css & make js & make server & wait)
--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
-window.onload = function() {
-  console.log("page loaded");
+var App = React.createClass({
+  render: function() {
+    return (
+
+      <div className="site">
+        <div className="bg">
+          <div className="bg__img">
+          </div>
+        </div>
+        <div className="site__main">
+          <div className="site__left-sidebar">
+          Test
+            {/* <SiteTitle/> */}
+          </div>
+          <div className="site__content">
+          Test2
+            {/* <Products/> */}
+          </div> {/* site__content */}
+        </div> {/* site__main */}
+        <div className="site__right-sidebar">
+        Test3
+          {/* <Cart/> */}
+          {/* <Checkout/> */}
+        </div> {/* site__right-sidebar */}
+        <a className="site__right-sidebar-toggle">
+          <img src="img/arrow-icon.svg" />
+        </a>
+      </div>
+    );
+  }
+});
+
+window.onload = () => {
+    // 使用 App 组件替换 `#root` 的 innerHTML。
+    React.render(<App />,document.querySelector("#root"));
+    console.log("app loaded.")
 }
